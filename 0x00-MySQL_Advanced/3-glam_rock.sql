db49adedef6bf63273d5fed18f5d474ce570a4bd
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-10-16 10:42:17 +0100

    ranking #3

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,5 @@
+-- List all badns woth Glam rock as their main style
+SELECT band_name AS band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY band_name ASC;
