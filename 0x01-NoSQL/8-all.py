c22de607adcf47f9a8e96ec11a0036f0634e5f1c
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-10-21 17:02:10 +0100

    list all documents in a collection #8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -2,6 +2,6 @@
 """" list all documents in a collection of a database """
 
 
-def list_all(mongo_collection):
+def list_all(mongo_collection: object) -> list:
     """ list all databases in MongoDB """
-    return mongo_collection.list_database_names()
+    return mongo_collection.find()
