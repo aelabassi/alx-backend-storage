6657d41fd472d94467ea0eb6c2df4159e8c6199e
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-10-22 14:45:58 +0100

    log stats #102

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+""" log stats from the collection """
+from pymongo import MongoClient
+
+
+def log_stats():
+    """ log stats from the collection
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs_collection = client.logs.nginx
+    total = logs_collection.count_documents({})
+    get = logs_collection.count_documents({"method": "GET"})
+    post = logs_collection.count_documents({"method": "POST"})
+    put = logs_collection.count_documents({"method": "PUT"})
+    patch = logs_collection.count_documents({"method": "PATCH"})
+    delete = logs_collection.count_documents({"method": "DELETE"})
+    path = logs_collection.count_documents(
+        {"method": "GET", "path": "/status"})
+    print(f"{total} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {get}")
+    print(f"\tmethod POST: {post}")
+    print(f"\tmethod PUT: {put}")
+    print(f"\tmethod PATCH: {patch}")
+    print(f"\tmethod DELETE: {delete}")
+    print(f"{path} status check")
+    ips_sorted = logs_collection.aggrgate([
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}}
+    ])
+    i = 0
+    for ip in ips_sorted:
+        if i == 10:
+            break
+        print(f"\t{ip.get('_id')}: {ip.get('count')}")
+        i += 1
+
+
+if __name__ == "__main__":
+    log_stats()
