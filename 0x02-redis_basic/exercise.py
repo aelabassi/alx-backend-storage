1777603de0086c451e27398ef7577cf79d2c8cfb
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-10-23 10:59:51 +0100

    Ccher: set #0

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+""" Cache class  """
+import redis
+import uuid
+from typing import Union, Callable
+from functools import wraps
+
+
+class Cache:
+    """ Cache class """
+    def __init__(self):
+        """ Constructor """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ Store data in redis """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
+
+    def get(self, key: str, fn: Callable = None) \
+            -> Union[str, bytes, int, float]:
+        """ Get data from redis """
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, data: bytes) -> str:
+        """ Convert bytes to str """
+        return data.decode('utf-8')
+
+    def get_int(self, data: bytes) -> int:
+        """ Convert bytes to int """
+        return int.from_bytes(data, byteorder='big')
+
+    def get_str_upper(self, data: bytes) -> str:
+        """ Convert bytes to str and return it in uppercase """
+        return data.decode('utf-8').upper()
+
+    def __call__(self, fn: Callable):
+        """ Call instance """
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            key = str(uuid.uuid4())
+            data = self._redis.get(key)
+            if data:
+                return data
+            data = fn(*args, **kwargs)
+            self._redis.set(key, data)
+            return data
+        return wrapper
