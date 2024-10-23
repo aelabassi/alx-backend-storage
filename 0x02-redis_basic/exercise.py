58415e6ed67018c4c5d4d0a879494241785c2f7c
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-10-23 11:28:39 +0100

    retrieving lists #4

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,23 +2,8 @@
 """ Cache class  """
 import redis
 import uuid
-from typing import Union, Callable
+from typing import Union, Callable, Optional
 from functools import wraps
-
-
-def replay(method: Callable) -> Callable:
-    """ Replay decorator """
-    self = method.__self__
-    key = method.__qualname__
-    count = self.get(key)
-    inputs = self.get(f"{key}:inputs")
-    outputs = self.get(f"{key}:outputs")
-    print(f"{key} was called {count} times:")
-    for i, (input, output) in enumerate(zip(inputs, outputs)):
-        print(f"{key}(*{input}) -> {output}")
-        if i == count - 1:
-            break
-    return method
 
 
 def count_calls(method: Callable) -> Callable:
@@ -57,10 +42,10 @@ class Cache:
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ Store data in redis """
         key = str(uuid.uuid4())
-        self._redis.set(key, data)
+        self._redis.mset(key, data)
         return key
 
-    def get(self, key: str, fn: Callable = None) \
+    def get(self, key: str, fn: Optional[Callable] = None) \
             -> Union[str, bytes, int, float]:
         """ Get data from redis """
         data = self._redis.get(key)
